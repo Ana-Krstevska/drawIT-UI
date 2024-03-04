10657c8e094756d2c3a848d8438876a2c5796efb
Ana-Krstevska <krstevska.ana@protonmail.com>
2024-03-04 18:03:08 +0100

    Implementing logic for swithcing themes. Applying it across elements

--- drawIT/src/app/components/search-bar/search-bar.component.ts
+++ drawIT/src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/services/themeService';
+import { Subscription } from 'rxjs'; 
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  borderColor = 'blue';  // Default color  
+  private subscription!: Subscription;  
+  
+  constructor(private themeService: ThemeService) { }  
+  
+  ngOnInit() {  
+    this.subscription = this.themeService.theme$.subscribe(theme => {  
+      this.borderColor = theme === 'azure' ? 'blue' : 'orange';  
+    });  
+  }  
+  
+  ngOnDestroy() {  
+    this.subscription.unsubscribe();  
+  } 
 
 }
