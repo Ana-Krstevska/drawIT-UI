5d3d95bb2b53e9e5efe12621944daed4532c210c
Ana-Krstevska <krstevska.ana@protonmail.com>
2024-04-01 21:36:22 +0200

    Making description text to be size responsive

--- drawIT/src/app/components/description-area/description-area.component.ts
+++ drawIT/src/app/components/description-area/description-area.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-description-area',
   templateUrl: './description-area.component.html',
   styleUrls: ['./description-area.component.scss']
 })
-export class DescriptionAreaComponent implements OnInit {
+export class DescriptionAreaComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('description') description!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  ngAfterViewInit(): void {
+    const textarea = this.description.nativeElement;
+    const lineHeight = parseFloat(window.getComputedStyle(textarea)["lineHeight"]);
+
+    textarea.style.overflowY = 'hidden';
+    textarea.style.height = (2 * lineHeight) + 'px';
+
+    textarea.addEventListener('input', () => {
+      const lines = Math.floor(textarea.scrollHeight / lineHeight);
+
+      if (textarea.value === '') {
+        textarea.style.height = (2 * lineHeight) + 'px';
+        textarea.style.overflowY = 'hidden';
+      } else if (lines === 2) {
+        textarea.style.height = (2 * lineHeight) + 'px';
+        textarea.style.overflowY = 'hidden';
+      } else if (lines <= 3) {
+        textarea.style.height = (lines * lineHeight) + 'px';
+        textarea.style.overflowY = 'hidden';
+      } else {
+        textarea.style.height = (3 * lineHeight) + 'px';
+        textarea.style.overflowY = 'auto';
+      }
+    });
+  }
+
+}  
