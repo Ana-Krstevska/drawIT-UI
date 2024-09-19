e4fe947715b91ecb4c96dee5af5a3beda26a350c
Ana-Krstevska <krstevska.ana@protonmail.com>
2024-09-19 18:46:03 +0200

    Ingesting cloud service names and showing them in search bar.

--- drawIT/src/app/input-page/input-page.component.ts
+++ drawIT/src/app/input-page/input-page.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit } from '@angular/core';  
-import { DrawingAPIService } from '../services/drawing-api.service';  
+import { Component } from '@angular/core';  
   
 @Component({  
   selector: 'app-input-page',  
   templateUrl: './input-page.component.html',  
   styleUrls: ['./input-page.component.scss']  
 })  
-export class InputPageComponent implements OnInit {  
-  title = 'drawIT';  
-  data = null;  
-  
-  constructor(private apiService: DrawingAPIService) {}  
-  
-  ngOnInit() {  
-    this.apiService.getAzureServices().subscribe((data: any) => {    
-      console.log(data);  
-      this.data = data;    
-    });   
-  }     
+export class InputPageComponent {  
+  title = 'drawIT';    
 }  
